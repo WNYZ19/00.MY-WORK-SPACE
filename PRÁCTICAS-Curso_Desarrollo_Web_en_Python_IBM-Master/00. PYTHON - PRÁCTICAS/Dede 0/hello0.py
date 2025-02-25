1fe561b17e0183ebe6820907a9130a664e4ef4b4
WNYZ19 <19wnyz@gmail.com>
2025-02-25 23:36:33 +0100

    Más material Python

--- PRÁCTICAS-Curso_Desarrollo_Web_en_Python_IBM-Master/00. PYTHON - PRÁCTICAS/Dede 0/hello0.py
+++ PRÁCTICAS-Curso_Desarrollo_Web_en_Python_IBM-Master/00. PYTHON - PRÁCTICAS/Dede 0/hello0.py
@@ -1,2 +1,15 @@
-#Mi primer Hola Mundo!
+# Mi primer Hola Mundo!
+# Esto es un comentario
 print("Hola Python!")
+print('Esto se llama cadena de texto')
+print('También se pueden utilizar comillas simples')
+"""
+Estes es
+un comentario
+en varias líneas 
+Por eso hay
+3 comillas delante y
+3 comillas detrás"""
+
+
+
